795d9a12d6f5bbe95ad343161da88395113df45d
Gabriel Klass <gk2533@columbia.edu>
2024-05-02 22:37:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def submit_feedback():
     return render_template('thanks.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+    app.run(debug=True,  host='0.0.0.0', port=4000)
